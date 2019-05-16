e206eef9b2377d183add9f18857673707a749eac
Karen Alonso <ingkaren.alonso@gmail.com>
2019-05-16 16:30:32 +0100

    Added loader

---
+++ portfolio/scripts/loading.js
@@ -0,0 +1,20 @@
+/* Spinner behaviour: Spinner will last 2 seconds, then an animation will reveal the screen and then main content */
+
+$(function() {
+  $(".home-container").hide();
+  animationHiText();
+
+  setTimeout(function(){
+    $(".loader-home").fadeOut("slow", function() {
+      //$(".video-background").addClass("home-effects");
+      $(".home-container").show();
+    });
+  }, 2800);
+
+  function animationHiText() {
+    setTimeout(function() {
+      $( ".hola-text" ).toggle();
+      animationHiText();
+     }, 1500);
+  }
+});
